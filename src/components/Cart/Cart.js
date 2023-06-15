28ca8c7a4050bc8d5d9926d2d6ec471642150dfc
SofPan <84824782+SofPan@users.noreply.github.com>
2023-06-15 17:32:47 -0400

    initial app & components setup

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './Cart.css';
+
+const Cart = props => {
+
+}
+
+export default Cart;
---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './Modal.css';
+
+const Modal = props => {
+    return <div className="modal"></div>
+}
+
+export default Modal;
