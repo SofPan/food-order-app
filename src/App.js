5360746a70e0a308f93b243ab66048fcd0266ed0
SofPan <84824782+SofPan@users.noreply.github.com>
2023-06-24 14:45:23 -0400

    Refactor Menu component to include menu items array and leaner jsx

--- src/App.js
+++ src/App.js
@@ -4,47 +4,12 @@ import Header from './components/UI/Header/Header';
 import './index.css';
 import CartProvider from './context/CartProvider';
 
-const MENU_ITEMS = [
-  {
-    id: "00",
-    title: "Premium mixed box",
-    description: "48 pieces of our top-tier chocolates",
-    cost: 57
-  },
-  {
-    id: "01",
-    title: "White Chocolate Truffle",
-    description: "12 pieces of Heaven in one box",
-    cost: 23.50
-  },
-  {
-    id: "02",
-    title: "Bag of Chocolate Covered Almonds",
-    description: "2 pounds of our premium roasted almonds coated in milk chocolate",
-    cost: 18.99
-  },
-  {
-    id: "03",
-    title: "Mystery Mix",
-    description: "Includes classic favorites and new delights",
-    cost: 15
-  },
-]
-
 function App() {
-  // const [cartItems, setCartItems] = useState([]);
-
-  // const addToCart = (item) => {
-  //   setCartItems((prevItems) => {
-  //     return [...prevItems, item];
-  //   });
-    
-  // }
 
   return (
     <CartProvider>
       <Header />
-      <Menu items={MENU_ITEMS}/>
+      <Menu />
     </CartProvider>
   );
 }
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,22 +1,59 @@
 import React from "react";
-import './Menu.css';
+import "./Menu.css";
 import MenuItem from "./MenuItem";
 
-const Menu = props => {
-    const newCartItem = (item) => {
-        props.addToCart(item);
-    }
-    return(
-        <div className="menu">
-            <div className="menu-inner">
-                <ul className="menu-item__list">
-                    {props.items.map((item) => {
-                        return <MenuItem key={item.id} newCartItem={newCartItem} id={item.id} title={item.title} description={item.description} cost={item.cost} />
-                    })}
-                </ul>
-            </div>
-        </div>
-    )
-}
+const MENU_ITEMS = [
+	{
+		id: "00",
+		title: "Premium mixed box",
+		description: "48 pieces of our top-tier chocolates",
+		cost: 57,
+	},
+	{
+		id: "01",
+		title: "White Chocolate Truffle",
+		description: "12 pieces of Heaven in one box",
+		cost: 23.5,
+	},
+	{
+		id: "02",
+		title: "Bag of Chocolate Covered Almonds",
+		description:
+			"2 pounds of our premium roasted almonds coated in milk chocolate",
+		cost: 18.99,
+	},
+	{
+		id: "03",
+		title: "Mystery Mix",
+		description: "Includes classic favorites and new delights",
+		cost: 15,
+	},
+];
+
+const Menu = () => {
+	const populateMenu = (
+		<ul className='menu-item__list'>
+			{MENU_ITEMS.map((item) => {
+				return (
+					<MenuItem
+						key={item.id}
+						id={item.id}
+						title={item.title}
+						description={item.description}
+						cost={item.cost}
+					/>
+				);
+			})}
+		</ul>
+	);
+
+	return (
+		<div className='menu'>
+			<div className='menu-inner'>
+                {populateMenu}
+			</div>
+		</div>
+	);
+};
 
 export default Menu;
