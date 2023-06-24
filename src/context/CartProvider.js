0e6cc55af2ad4593c61eabd9e027027e8b59da17
SofPan <84824782+SofPan@users.noreply.github.com>
2023-06-24 14:57:50 -0400

    Block in add and remove cart handler functions in CartProvider

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -1,10 +1,29 @@
 import CartContext from "./CartContext"
 
+const defaultCart = {
+    cartItems: [],
+    totalAmount: 0
+}
 
 const CartProvider = props => {
 
+    
+
+    const handleAddToCart = (item) => {
+
+    };
+
+    const handleRemoveFromCart = (id) => {
+
+    }
+    const crtContext = {
+        cartItems: [],
+        totalAmount: 0,
+        onAddToCart: handleAddToCart,
+        onRemoveFromCart: handleRemoveFromCart
+    }
     return(
-        <CartContext.Provider>
+        <CartContext.Provider value={crtContext}>
             {props.children}
         </CartContext.Provider>
     )
